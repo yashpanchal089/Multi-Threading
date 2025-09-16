be78b177a52b814afd81c8a1fce53e712b30aac9
Yash Panchal <panchalyash089@gmail.com>
2025-09-16 12:46:03 +0530

    Multiple threads talk and coordinate with each other

---
+++ MultiThreading/src/ThreadCommunication.java
@@ -0,0 +1,17 @@
+
+
+class SharedResource {
+    private int data;
+    private boolean hasData;
+
+    public void produce(){
+
+    }
+    public void consume(){
+
+    }
+}
+
+
+public class ThreadComunication {
+}
