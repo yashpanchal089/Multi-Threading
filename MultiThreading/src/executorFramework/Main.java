75620ec242ad1bee3eb982226ce9f0ceb6229123
Yash Panchal <panchalyash089@gmail.com>
2025-09-17 12:16:41 +0530

    Added ExecutorService example demonstrating task submission and result

---
+++ MultiThreading/src/executorFramework/Main.java
@@ -0,0 +1,35 @@
+package executorFramework;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        ExecutorService executor = Executors.newFixedThreadPool(9);
+
+        for (int  i = 1; i < 10; i++){
+            int finalI = i;
+            executor.submit(() -> {
+                long result = factorial(finalI);
+                System.out.println(result);
+            });
+
+        }
+
+        System.out.println("Total time : "+ (System.currentTimeMillis() - startTime));
+    }
+
+    private static long factorial(int n){
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        long result = 1;
+        for (int i = 1; i<= n; i++){
+            result *= i;
+        }
+        return result;
+    }
+}
